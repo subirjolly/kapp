8f1d1aad3c3f1c931979ebe82e668a5e4cc04176
Subir Jolly <subir.jolly@rackspace.com>
2016-12-27 12:48:53 -0500

    Update.

--- backup/server_runner.py
+++
@@ -1,8 +0,0 @@
-from broker import Broker
-from worker import WorkerConfig
-
-host = "secure.emailsrvr.com"
-port = 143
-
-worker_config = WorkerConfig(host, port)
-Broker("localhost", 1430, worker_config).start()
